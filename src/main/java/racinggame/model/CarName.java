695b1b3cb76a785715983674d7580b5f8789ade3
kdh85 <modudamba@naver.com>
2021-10-07 16:42:16 +0900

    feat : 자동차게임 이름객체 중복여부 판단 추가 * 객체 비교를 통하여 동일한 이름객체인지 여부를 비교하는 메소드 추가.

--- src/main/java/racinggame/model/CarName.java
+++ src/main/java/racinggame/model/CarName.java
@@ -25,6 +25,10 @@ public class CarName {
 		}
 	}
 
+	public boolean isSameName(CarName carName) {
+		return this.equals(carName);
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o)
