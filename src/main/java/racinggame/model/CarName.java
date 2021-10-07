c518fe397948dbe74327ed202780718d485209de
kdh85 <modudamba@naver.com>
2021-10-07 16:18:34 +0900

    feat : 자동차게임 차이름 객체 생성 * 문자열을 입력받아 차량이름 객체를 생성. * 입력받은 이름이 빈값 이거나, 길이가 5를 초과하면 에러를 발생.

---
+++ src/main/java/racinggame/model/CarName.java
@@ -0,0 +1,49 @@
+package racinggame.model;
+
+import java.util.Objects;
+
+public class CarName {
+
+	private static final int MAX_CAR_NAME_LENGTH = 5;
+	private static final String MSG_ERROR_NAME_LENGTH = String.format("차량이름은 최대 %d글자까지 가능합니다.", MAX_CAR_NAME_LENGTH);
+	private static final String MSG_ERROR_EMPTY_NAME = "차량이름이 비었습니다. 이름을 입력해주세요.";
+
+	private final String carName;
+
+	public CarName(String carName) {
+		validationCarName(carName);
+		this.carName = carName;
+	}
+
+	private void validationCarName(String carName) {
+		if (carName == null || carName.isEmpty()) {
+			throw new IllegalArgumentException(MSG_ERROR_EMPTY_NAME);
+		}
+
+		if (carName.length() > MAX_CAR_NAME_LENGTH) {
+			throw new IllegalArgumentException(MSG_ERROR_NAME_LENGTH);
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		CarName carName1 = (CarName)o;
+		return Objects.equals(carName, carName1.carName);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(carName);
+	}
+
+	@Override
+	public String toString() {
+		return "CarName{" +
+			"carName='" + carName + '\'' +
+			'}';
+	}
+}
