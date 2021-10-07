7532e3e1573bc24528f398496d42c66321c4210b
kdh85 <modudamba@naver.com>
2021-10-07 17:01:01 +0900

    feat : 자동차게임 차 객체 생성 * 차량이름을 주입받으면 거리0인 차 객체를 생성.

---
+++ src/main/java/racinggame/model/Car.java
@@ -0,0 +1,42 @@
+package racinggame.model;
+
+import java.util.Objects;
+
+public class Car {
+
+	private final CarName carName;
+
+	private final Distance distance;
+
+	public Car(final String carName) {
+		this(new CarName(carName), new Distance());
+	}
+
+	public Car(final CarName carName, final Distance distance) {
+		this.carName = carName;
+		this.distance = distance;
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		final Car car = (Car)o;
+		return Objects.equals(carName, car.carName) && Objects.equals(distance, car.distance);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(carName, distance);
+	}
+
+	@Override
+	public String toString() {
+		return "Car{" +
+			"carName=" + carName +
+			", distance=" + distance +
+			'}';
+	}
+}
