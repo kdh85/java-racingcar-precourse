30a3c6af241eee2ccbab37b7a466ec4afd7081a6
kdh85 <modudamba@naver.com>
2021-10-07 17:30:39 +0900

    test : 자동차게임 차량명 중복여부 메소드 테스트코드 작성 * 차객체간 차량명이 같은지 여부를 확인하는 메소드 테스트코드 작성.

--- src/test/java/racinggame/model/CarTest.java
+++ src/test/java/racinggame/model/CarTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CarTest {
 
@@ -18,13 +20,20 @@ class CarTest {
 	@DisplayName("차를 처음 생성할때 거리0인 차객체를 생성한다.")
 	@Test
 	void createCarTest() {
-		assertThat(car).isEqualTo(new Car(new CarName("car"),new Distance(0)));
+		assertThat(car).isEqualTo(new Car(new CarName("car"), new Distance(0)));
 	}
 
 	@DisplayName("차가 이동할때 거리객체 이동 메세지를 전달하면 거리1만큼 이동된다.")
 	@Test
 	void moveTheCarTest() {
 		car.moveTheCar();
-		assertThat(car).isEqualTo(new Car(new CarName("car"),new Distance(1)));
+		assertThat(car).isEqualTo(new Car(new CarName("car"), new Distance(1)));
+	}
+
+	@DisplayName("차량생성시 중복된 차량이름이 입력시 판별값을 반환한다.")
+	@ParameterizedTest
+	@CsvSource(value = {"car|true", "cars|false"}, delimiter = '|')
+	void duplicateCarTest(String name, boolean isSame) {
+		assertThat(car.isSameName(new Car(name))).isEqualTo(isSame);
 	}
 }
