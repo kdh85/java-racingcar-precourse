61fedae1db10c7e274026f536dd3da974aee5813
kdh85 <modudamba@naver.com>
2021-10-08 15:29:13 +0900

    test : 자동차게임 차객체 생성자 추가 및 최대거리와 같은 객체 비교 테스트코드 및 코드 리팩토링 * 생성자 추가로 인한 기존 테스트코드 리팩토링. * 대상차의 거리와 동일한 거리의 차인지 비교하는 메소드 테스트코드 작성.

--- src/test/java/racinggame/model/CarTest.java
+++ src/test/java/racinggame/model/CarTest.java
@@ -21,14 +21,14 @@ class CarTest {
 	@DisplayName("차를 처음 생성할때 거리0인 차객체를 생성한다.")
 	@Test
 	void createCarTest() {
-		assertThat(car).isEqualTo(new Car(new CarName("car"), new Distance(0)));
+		assertThat(car).isEqualTo(new Car("car", 0));
 	}
 
 	@DisplayName("차가 이동할때 거리객체 이동 메세지를 전달하면 거리1만큼 이동된다.")
 	@Test
 	void moveTheCarTest() {
 		car.moveTheCar();
-		assertThat(car).isEqualTo(new Car(new CarName("car"), new Distance(1)));
+		assertThat(car).isEqualTo(new Car("car", 1));
 	}
 
 	@DisplayName("차량생성시 중복된 차량이름이 입력시 판별값을 반환한다.")
@@ -46,8 +46,15 @@ class CarTest {
 		Car result = this.car.maxDistanceCar(targetCar);
 
 		assertAll(
-			()-> assertThat(result).isEqualTo(this.car),
-			()-> assertThat(result.carName()).isEqualTo("car")
+			() -> assertThat(result).isEqualTo(this.car),
+			() -> assertThat(result.carName()).isEqualTo("car")
 		);
+	}
+
+	@DisplayName("차량 객체간 이동거리가 같은지 비교를 한다.")
+	@ParameterizedTest
+	@CsvSource(value = {"b|0|true", "b|1|false"}, delimiter = '|')
+	void sameDistanceTest(String name, int distance, boolean isSame) {
+		assertThat(car.isSameDistance(new Car(name, distance))).isEqualTo(isSame);
 	}
 }
