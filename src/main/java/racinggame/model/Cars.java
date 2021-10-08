18bff69656858d60f919e718fbae57d138b94efe
kdh85 <modudamba@naver.com>
2021-10-08 11:10:58 +0900

    refactor : 자동차게임 차들객체 생성자 리팩토링 * 차량이름들을 주입받아 차량들 객체를 생성하는 생성자를 추가함. * 주입받은 차량명을 특수문자 쉼표 단위로 나눠서 차량이름에 할당한 차객체를 생성.

--- src/main/java/racinggame/model/Cars.java
+++ src/main/java/racinggame/model/Cars.java
@@ -1,15 +1,36 @@
 package racinggame.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 public class Cars {
 
+	private static final String SPLIT_SEPARATOR = ",";
+
 	private final List<Car> cars;
+
+	public Cars(final String names) {
+		this.cars = Collections.unmodifiableList(createNewCars(names));
+	}
 
 	public Cars(final List<Car> cars) {
 		this.cars = Collections.unmodifiableList(cars);
+	}
+
+	private List<Car> createNewCars(final String names) {
+		List<Car> newCars = new ArrayList<>();
+
+		for (String carName : splitNames(names)) {
+			newCars.add(new Car(carName));
+		}
+		return newCars;
+	}
+
+	private static List<String> splitNames(final String names) {
+		return Arrays.asList(names.split(SPLIT_SEPARATOR));
 	}
 
 	@Override
@@ -33,4 +54,5 @@ public class Cars {
 			"cars=" + cars +
 			'}';
 	}
+
 }
