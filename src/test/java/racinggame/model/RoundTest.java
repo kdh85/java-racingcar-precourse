0e6cc43f6199d1eb76398ac4b155554335ebe953
kdh85 <modudamba@naver.com>
2021-10-09 13:19:55 +0900

    test : 자동차게임 라운드객체 테스트코드 작성 * 시행횟수를 관리하는 개념의 라운드 객체 테스트코드 작성. * 라운드 최소 입력값에 대한 검증 테스트코드 작성. * 최대 라운드값까지 라운드 진행에 따른 현재의 라운드값 검증 테스트코드 작성.

---
+++ src/test/java/racinggame/model/RoundTest.java
@@ -0,0 +1,42 @@
+package racinggame.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RoundTest {
+	private Round round;
+
+	@BeforeEach
+	void setUp() {
+		round = new Round(1);
+	}
+
+	@DisplayName("정상적으로 라운드값이 증가되는지 확인한다.")
+	@Test
+	void increaseRound() {
+		assertThat(round.increaseRound()).isEqualTo(1);
+	}
+
+	@DisplayName("1 미만의 값으로 라운드가 생성되면 에러가 발생한다.")
+	@Test
+	void validationRound() {
+		assertThatThrownBy(
+			() -> new Round(0)
+		).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("현재의 라운드값이 총 라운드값과 같지 않은지 비교한다.")
+	@Test
+	void isSameRound() {
+		assertThat(round.isNotSameRound()).isTrue();
+	}
+
+	@DisplayName("입력받은 최대 라운드값을 반환한다.")
+	@Test
+	void maxRound() {
+		assertThat(round.maxRound()).isEqualTo(1);
+	}
+}
