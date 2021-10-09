1450e1adc02b9654f1f49253ab8d7a8e1f137b8e
kdh85 <modudamba@naver.com>
2021-10-09 13:19:28 +0900

    feat : 자동차게임 라운드객체 추가 * 시행횟수를 관리하는 개념의 라운드 객체 추가. * 라운드 최소 입력값에 대한 검증. * 최대 라운드값까지 라운드 진행에 따른 현재의 라운드값 증가.

---
+++ src/main/java/racinggame/model/Round.java
@@ -0,0 +1,60 @@
+package racinggame.model;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Round {
+
+	private static final int ROUND_VALUE = 1;
+	private static final String MSG_ERROR_MIN_ROUND = String.format("라운드값은 최소 %d부터 가능합니다.", ROUND_VALUE);
+
+	private final int round;
+
+	private final AtomicInteger currentRound = new AtomicInteger();
+
+	public Round(final int round) {
+		validationRound(round);
+		this.round = round;
+	}
+
+	private void validationRound(final int round) {
+		if (round < ROUND_VALUE) {
+			throw new IllegalArgumentException(MSG_ERROR_MIN_ROUND);
+		}
+	}
+
+	public int increaseRound() {
+		return currentRound.incrementAndGet();
+	}
+
+	public boolean isNotSameRound() {
+		return round != currentRound.get();
+	}
+
+	public int maxRound() {
+		return round;
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		final Round round1 = (Round)o;
+		return round == round1.round && Objects.equals(currentRound, round1.currentRound);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(round, currentRound);
+	}
+
+	@Override
+	public String toString() {
+		return "Round{" +
+			"round=" + round +
+			", currentRound=" + currentRound +
+			'}';
+	}
+}
