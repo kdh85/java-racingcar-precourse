eb2699d29f241c7c01fe7630622129a20dca6bbc
kdh85 <modudamba@naver.com>
2021-10-09 14:15:49 +0900

    feat : 자동차게임 경기경주객체 생성 * 경기 라운드와 자동차 콜랙션을 지닌 경기경주 객체를 정의함. * 이동거리 메세지에 대한 기준을 정의하는 메소드 추가. * 경기 경주 한 라운드에 대한 경주결과를 반환하는 메소드 추가.

---
+++ src/main/java/racinggame/model/Racing.java
@@ -0,0 +1,82 @@
+package racinggame.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import nextstep.utils.Randoms;
+
+public class Racing {
+
+	private static final int START_NUMBER = 0;
+	private static final int END_NUMBER = 9;
+	private static final int MOVABLE_NUMBER = 4;
+
+	private final Round round;
+
+	private final Cars cars;
+
+	public Racing(final int round, final String names) {
+		this(new Round(round), new Cars(names));
+	}
+
+	public Racing(final Round round, final Cars cars) {
+		this.round = round;
+		this.cars = cars;
+	}
+
+	public void play() {
+		cars.moveEachCars(choiceMovingCars());
+	}
+
+	private List<Integer> choiceMovingCars() {
+		List<Integer> moveCarIndex = new ArrayList<>();
+
+		AtomicInteger index = new AtomicInteger();
+
+		while (index.intValue() < cars.carCount()) {
+			addMovingCar(moveCarIndex, index.getAndIncrement());
+		}
+
+		return moveCarIndex;
+	}
+
+	private void addMovingCar(final List<Integer> moveCarIndex, final int carIndex) {
+		if (isMovable()) {
+			moveCarIndex.add(carIndex);
+		}
+	}
+
+	private boolean isMovable() {
+		return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER) >= MOVABLE_NUMBER;
+	}
+
+	public Map<String,Integer> racingResult(){
+		return cars.result();
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		final Racing racing = (Racing)o;
+		return round == racing.round && Objects.equals(cars, racing.cars);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(round, cars);
+	}
+
+	@Override
+	public String toString() {
+		return "Racing{" +
+			"round=" + round +
+			", cars=" + cars +
+			'}';
+	}
+}
