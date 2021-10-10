ba9ec40822bfe9a468fba3fff776a19c9ba82d01
kdh85 <modudamba@naver.com>
2021-10-10 15:23:24 +0900

    feat : 자동차게임 게임 실행 구현 * 게임실행을 하기 위한 입력값 생성 및 경주 객체에 값 주입 구현. * 경기 라운드별 레이싱 진행상황 출력. * 경기 최종 우승자 출력.

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,33 @@
 package racinggame;
 
+import racinggame.model.Racing;
+import racinggame.view.InputView;
+import racinggame.view.ResultView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 자동차 경주 게임 구현
-    }
+
+	private static final String MSG_INSERT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String MSG_INSERT_MAX_ROUND = "시도할 회수는 몇회인가요?";
+
+	public static void main(String[] args) {
+		
+		String names = InputView.inputData(MSG_INSERT_CAR_NAMES);
+		String maxRound = InputView.inputData(MSG_INSERT_MAX_ROUND);
+
+		Racing racing = new Racing(toInt(maxRound), names);
+
+		ResultView.printResultTitle();
+
+		while (racing.isContinue()) {
+			racing.play();
+
+			ResultView.printRacingProgress(racing.racingResult());
+		}
+
+		ResultView.printWinner(racing.winner());
+	}
+
+	private static int toInt(final String maxRound) {
+		return Integer.parseInt(maxRound);
+	}
 }
